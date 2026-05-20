845f319141602d7cba51b10400eb940c717c4b02
¨Jael <ljael1997@gmail.com>
2026-05-20 10:20:45 -0600

    Agregue animacion_sustitucion

---
+++ animacion_sustitucion.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def animacion_sustitucion():
+    st.subheader("📝 Misión: Sistemas de Ecuaciones 2x2 (Sustitución)")
+    st.write("¡Aquí irá tu juego de sustitución de ecuaciones!")
